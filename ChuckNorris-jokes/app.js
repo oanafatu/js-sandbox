98e9a7a5e6c3cb09efeea03516fa701408492a4c
Oana Fatu <oana.fatu@icloud.com>
2019-08-28 14:46:37 +0200

    https

--- ChuckNorris-jokes/app.js
+++ ChuckNorris-jokes/app.js
@@ -5,7 +5,7 @@ function getJokes(e) {
   const numberOfJokes = document.querySelector('#number').value;
 
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', `http://api.icndb.com/jokes/random/${numberOfJokes}`, true)
+  xhr.open('GET', `https://api.icndb.com/jokes/random/${numberOfJokes}`, true)
   xhr.onload = function () {
 
     if (this.status === 200) {
