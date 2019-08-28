bf8f79e744a945fa34e4dfae19de271e75cfce1a
Oana Fatu <oana.fatu@icloud.com>
2019-08-28 14:43:43 +0200

    chuck norris joke generator

---
+++ ChuckNorris-jokes/app.js
@@ -0,0 +1,29 @@
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e) {
+
+  const numberOfJokes = document.querySelector('#number').value;
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${numberOfJokes}`, true)
+  xhr.onload = function () {
+
+    if (this.status === 200) {
+      let output = '';
+      const response = JSON.parse(this.responseText);
+      if (response.type === 'success') {
+        
+          response.value.forEach(joke => {
+            output += `<li>${joke.joke}</li>`;
+          })
+       
+      } else {
+        output += '<li>Something went wrong</li>' 
+      }
+      document.querySelector('.jokes').innerHTML = output;
+    }
+  };
+  xhr.send();
+
+  e.preventDefault(e);
+}
