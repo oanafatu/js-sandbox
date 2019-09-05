0229daecf756fdf2634cfa7875bb443a3b5d17cd
Oana Fatu <oana.fatu@icloud.com>
2019-09-05 10:47:29 +0200

    fetch API

---
+++ fetch-API/app.js
@@ -0,0 +1,47 @@
+document.getElementById('button1').addEventListener('click', getText);
+
+function getText(){
+  fetch('test.txt')
+    .then(res => res.text())
+    .then(data => {
+     
+      document.getElementById('output').innerHTML = data;
+    }
+      )
+    .catch(err => console.log(err))
+}
+
+document.getElementById('button2').addEventListener('click', getJSON);
+
+function getJSON(){
+  fetch('post.json')
+    .then(res => res.json())
+    .then(data => {
+    
+      let output = '';
+      data.forEach(post => {
+        output += `<li>${post.title}: ${post.body}</li>`;
+      });
+      document.getElementById('output').innerHTML = output;
+    }
+      )
+    .catch(err => console.log(err))
+}
+
+document.getElementById('button3').addEventListener('click', getExternal);
+
+function getExternal(){
+  fetch('https://api.github.com/users')
+    .then(res => res.json())
+    .then(data => {
+     
+      let output = '';
+      data.forEach(user => {
+        output += `<li>${user.login}</li>`;
+      });
+      document.getElementById('output').innerHTML = output;
+    }
+      )
+    .catch(err => console.log(err))
+}
+
