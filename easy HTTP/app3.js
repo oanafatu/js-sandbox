4f908088a4777c119d293674815e388b364b4509
Oana Fatu <oana.fatu@icloud.com>
2019-09-05 11:57:53 +0200

    await async

---
+++ easy HTTP/app3.js
@@ -0,0 +1,25 @@
+const http = new EasyHTTP3;
+
+// http.get('https://jsonplaceholder.typicode.com/users')
+//   .then (data => console.log(data))
+//   .catch (err => console.log(err))
+
+const userData = {
+  name: 'John Doe',
+  username: 'johndoe',
+  email: 'jdoe@gmail.com'
+} 
+
+// http.post('https://jsonplaceholder.typicode.com/users', userData)
+//   .then (data => console.log(data))
+//   .catch(err => console.log(err))
+
+
+  
+// http.put('https://jsonplaceholder.typicode.com/users/2', userData)
+//   .then (data => console.log(data))
+//   .catch(err => console.log(err))
+
+http.delete('https://jsonplaceholder.typicode.com/users/2')
+.then (data => console.log(data))
+.catch(err => console.log(err))
